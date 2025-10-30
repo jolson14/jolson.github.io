7fc82cb0393b3645116a7ce34a79a8aa85d62fe5
jolson <jolson142000@gmail.com>
2025-10-30 15:41:53 -0500

    Finished lab 7 excluding the query part.

--- lab7_HW.js
+++ lab7_HW.js
@@ -1,6 +1,19 @@
 require(["esri/Map", "esri/views/MapView", "esri/layers/TileLayer"], function (Map, MapView, TileLayer) {
-var tileLayer = new TileLayer({
-  url: "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/ArcGIS/rest/services/2024_US_Census/FeatureServer"
-});
 
-map.add(tileLayer);
+    var map = new Map({             //adds basemap
+      basemap: "gray-vector"
+    });
+
+    var view = new MapView({        //adds new container for the basemap
+      container: "viewDiv",
+      map: map,
+      center: [-98, 45],
+      zoom: 2
+    });
+
+    var tileLayer = new TileLayer({   //tilelayer created with congressional districts in the USA
+      url: "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/ArcGIS/rest/services/Congressional_Districts/MapServer"
+    });
+
+    map.add(tileLayer);
+});
